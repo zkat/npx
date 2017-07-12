d531206f555d3ca761d62b2309c5641c8564f238
Kat Marchán <kzm@sykosomatic.org>
2017-07-11 19:44:14 -0700

    fix(npm): path directly to the npm-cli.js script
    I don't know if this fixes anything in particular, but after seeing
    recent issues on Windows, I think it'd be nicer to make sure this
    is consistent cross-platform, instead of sometimes hitting the cmd shim.

--- bin/index.js
+++ bin/index.js
@@ -3,7 +3,7 @@
 const npx = require('libnpx')
 const path = require('path')
 
-const NPM_PATH = path.join(__dirname, 'node_modules', '.bin', 'npm')
+const NPM_PATH = path.join(__dirname, 'node_modules', 'npm', 'bin', 'npm-cli.js')
 
 const parsed = npx.parseArgs(process.argv, NPM_PATH)
 parsed.npxPkg = path.join(__dirname, 'package.json')
--- test/util/npx-bin.js
+++ test/util/npx-bin.js
@@ -3,6 +3,6 @@
 const npx = require('../../index.js')
 const path = require('path')
 
-const NPM_PATH = path.join(__dirname, '../..', 'node_modules', '.bin', 'npm')
+const NPM_PATH = path.join(__dirname, '../..', 'node_modules', 'npm', 'bin', 'npm-cli.js')
 
 npx(npx.parseArgs(process.argv, NPM_PATH))
