073602e77dee495381456349b5fc7d6ca4a6ef59
Kat Marchán <kzm@sykosomatic.org>
2017-07-17 16:26:03 -0700

    nudge: fix up test stuff

--- test/index.js
+++ test/index.js
@@ -9,12 +9,16 @@ const test = require('tap').test
 
 const main = require('../index.js')
 
-let NPX_PATH = path.resolve(__dirname, 'util', 'npx-bin.js')
-let NPM_PATH = path.resolve(__dirname, '..', 'node_modules', 'npm', 'bin', 'npm-cli.js')
+const isWindows = process.platform === 'win32'
+
+const NPX_PATH = path.resolve(__dirname, 'util', 'npx-bin.js')
+const NPM_PATH = path.resolve(__dirname, '..', 'node_modules', 'npm', 'bin', 'npm-cli.js')
+
+const NPX_ESC = isWindows ? child.escapeArg(NPX_PATH) : NPX_PATH
 
 test('npx --shell-auto-fallback', t => {
   return child.spawn('node', [
-    child.escapeArg(NPX_PATH), '--shell-auto-fallback', 'zsh'
+    NPX_ESC, '--shell-auto-fallback', 'zsh'
   ], {stdio: 'pipe'}).then(res => {
     t.equal(res.code, 0, 'command succeeded')
     t.match(
@@ -25,7 +29,7 @@ test('npx --shell-auto-fallback', t => {
 
 test('npx no command', t => {
   return child.spawn('node', [
-    child.escapeArg(NPX_PATH)
+    NPX_ESC
   ], {stdio: 'pipe'}).then(res => {
     throw new Error('Should not have succeeded')
   }, err => {
@@ -40,10 +44,10 @@ test('npx no command', t => {
 })
 
 test('npx existing subcommand', {
-  skip: process.platform === 'win32' && 'Windows fail this test when run via nyc, but not when run directly'
+  skip: isWindows && 'Windows fail this test when run via nyc, but not when run directly'
 }, t => {
   return child.spawn('node', [
-    child.escapeArg(NPX_PATH), 'which', 'npm'
+    NPX_ESC, 'which', 'npm'
   ], {stdio: 'pipe'}).then(res => {
     t.notOk(res.stderr, 'no stderr output')
     t.ok(res.stdout.trim(), 'got output from command')
@@ -51,18 +55,18 @@ test('npx existing subcommand', {
 })
 
 test('execCommand unit', {
-  skip: process.platform === 'win32' && 'need a workaround for obnoxious auto-open of .md file on Windows'
+  skip: isWindows && 'need a workaround for obnoxious auto-open of .md file on Windows'
 }, t => {
   let whichBin = path.resolve(
     __dirname, '..', 'node_modules', '.bin', 'which'
   )
-  if (process.platform === 'win32') {
+  if (isWindows) {
     whichBin += '.CMD'
   }
   return main._execCommand(null, {
     command: path.resolve(__dirname, '..', 'README.md')
   }).then(res => {
-    if (process.platform === 'win32') {
+    if (isWindows) {
       t.equal(res.code, 0, 'cmd.exe opened the file via its associated executable and returned success')
     } else {
       throw new Error('should not have succeeded')
@@ -175,7 +179,7 @@ test('getNpmCache', t => {
 
 test('findNodeScript', t => {
   return main._findNodeScript(NPX_PATH).then(script => {
-    if (process.platform === 'win32') {
+    if (isWindows) {
       t.notOk(script, 'win32 never detects Node scripts like this')
     } else {
       t.equal(script, NPX_PATH, 'existing returned as-is on *nix')
@@ -216,7 +220,7 @@ test('findNodeScript', t => {
       }
     })._findNodeScript
     return findScript('fail').then(f => {
-      if (process.platform === 'win32') {
+      if (isWindows) {
         t.notOk(f, 'win32 gives no fucks')
       } else {
         throw new Error('should have failed')
